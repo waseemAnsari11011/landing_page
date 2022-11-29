376b19dda243a8828fbc3a308442b032c6905f10
waseemAnsari11011 <72751386+waseemAnsari11011@users.noreply.github.com>
2022-11-29 14:20:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ const publicDirectoryPath = path.join(__dirname, "./client");
 app.use(express.static(publicDirectoryPath));
 const port = process.env.PORT || 3600;
 
-mongoose.connect(
-  "mongodb+srv://eshop-user:1234567Ww@cluster0.m7qgjqu.mongodb.net/?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGOURL);
 
 const Emails = mongoose.model("Emails", {
   name: {
@@ -33,7 +31,7 @@ app.post("/formdata", async (req, res) => {
     service: "gmail",
     auth: {
       user: "waseemahm05@gmail.com",
-       pass: "gwppalxvtifivfvi",
+       pass: process.env.EMAILPASS,
     },
   });
 
